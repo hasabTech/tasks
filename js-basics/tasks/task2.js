160ce339c6fc84ff8f4c05526f97358bc96c0300
Arooba-Naveed <145915373+Arooba-Naveed@users.noreply.github.com>
2025-11-26 20:15:32 +0500

    TASK-JS-BASICS-02 completed

--- js-basics/tasks/task2.js
+++ js-basics/tasks/task2.js
@@ -1,5 +1,7 @@
 // Task 2: Create a constant PI with value 3.14 and export it.
 
 // TODO: Create a constant called 'PI' with value 3.14
-
+const PI = 3.14;
 // TODO: Export the PI constant
+module.exports = { PI };
+
