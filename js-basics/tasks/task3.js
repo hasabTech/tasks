78bdc7f8db5ac92c69af3ceb67eba6fbd836b9d6
Arooba-Naveed <145915373+Arooba-Naveed@users.noreply.github.com>
2025-11-26 20:28:02 +0500

    TASK-JS-BASICS-03 completed

--- js-basics/tasks/task3.js
+++ js-basics/tasks/task3.js
@@ -1,9 +1,12 @@
 // Task 3: Write a function add(a, b) that returns the sum of a and b.
 
 // TODO: Create a function called 'add' that takes two parameters (a, b)
+function add(a, b) {
 // TODO: Return the sum of a and b
+return a + b;
+}
 // function add(a, b) {
-//   // Your code here
+module.exports = { add };
 // }
 
 
