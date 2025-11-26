d0165a74a5bcb9c09b75c4cb8c6108af45c3c016
Arooba-Naveed <145915373+Arooba-Naveed@users.noreply.github.com>
2025-11-26 20:50:59 +0500

    TASK-JS-BASICS-05 completed

--- js-basics/tasks/task5.js
+++ js-basics/tasks/task5.js
@@ -1,15 +1,19 @@
 // Task 5: Write a function getGrade(score) that returns "A", "B", "C", "D", "F" based on score.
 
-// TODO: Create a function called 'getGrade' that takes one parameter (score)
-// TODO: Return grades based on the following scale:
-// 90-100: "A"
-// 80-89: "B" 
-// 70-79: "C"
-// 60-69: "D"
-// Below 60: "F"
+// Create a function called 'getGrade' that takes one parameter (score)
+function getGrade(score) {
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
 
-// function getGrade(score) {
-//   // Your code here using if-else statements
-// }
+module.exports = { getGrade };
 
-// TODO: Export the getGrade function
