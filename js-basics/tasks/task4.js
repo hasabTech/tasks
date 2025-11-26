33f66fc8289dab6708bbc033ba79c06963d27710
Arooba-Naveed <145915373+Arooba-Naveed@users.noreply.github.com>
2025-11-26 20:39:26 +0500

    TASK-JS-BASICS-04 completed

--- js-basics/tasks/task4.js
+++ js-basics/tasks/task4.js
@@ -1,7 +1,10 @@
 // Task 4: Write a function isEven(num) that returns true if number is even.
 
 // TODO: Create a function called 'isEven' that takes one parameter (num)
+function isEven(num){
 // TODO: Return true if the number is even, false if odd
+return num % 2 === 0;
+}
 // Hint: Use the modulo operator (%) to check if a number is divisible by 2
 
 // function isEven(num) {
@@ -9,4 +12,4 @@
 // }
 
 // TODO: Export the isEven function
-
+module.exports = { isEven };
