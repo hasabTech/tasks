5bda445f9da7c71feb1fe8e0227ce728257d3901
Arooba-Naveed <145915373+Arooba-Naveed@users.noreply.github.com>
2025-11-26 21:03:54 +0500

    TASK-JS-BASICS-07 completed

--- js-basics/tasks/task7.js
+++ js-basics/tasks/task7.js
@@ -1,11 +1,14 @@
 // Task 7: Write a function sumArray(arr) that returns the sum of all numbers in an array.
 
 // TODO: Create a function called 'sumArray' that takes one parameter (arr)
-// TODO: Use a loop to iterate through the array and sum all numbers
-// TODO: Return the total sum
+function sumArray(arr) {
+  let sum = 0;
 
-// function sumArray(arr) {
-//   // Your code here using a for loop or for...of loop
-// }
+  for (let num of arr) {
+    sum += num;
+  }
 
-// TODO: Export the sumArray function
+  return sum;
+}
+
+module.exports = { sumArray };
