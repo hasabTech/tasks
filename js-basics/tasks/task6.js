9162029f08352ed041b9b1d77ca134928e5e4fe5
Arooba-Naveed <145915373+Arooba-Naveed@users.noreply.github.com>
2025-11-28 17:27:12 +0500

    TASK-JS-BASICS-ALL completed

--- js-basics/tasks/task6.js
+++ js-basics/tasks/task6.js
@@ -1,10 +1,11 @@
 // Task 6: Write a function isAdult(age) that returns "Adult" if age >= 18 else "Minor".
 
-// TODO: Create a function called 'isAdult' that takes one parameter (age)
-// TODO: Return "Adult" if age is 18 or greater, otherwise return "Minor"
+function isAdult(age) {
+  if (age >= 18) {
+    return "Adult";
+  } else {
+    return "Minor";
+  }
+}
 
-// function isAdult(age) {
-//   // Your code here
-// }
-
-// TODO: Export the isAdult function
+module.exports = { isAdult };
--- js-basics/tasks/task8.js
+++ js-basics/tasks/task8.js
@@ -1,11 +1,9 @@
-// Task 8: Write a function factorial(n) that returns factorial of n.
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
 
-// TODO: Create a function called 'factorial' that takes one parameter (n)
-// TODO: Calculate and return the factorial of n (n! = n × (n-1) × (n-2) × ... × 1)
-// TODO: Remember that 0! = 1 and 1! = 1
-
-// function factorial(n) {
-//   // Your code here using a loop or recursion
-// }
-
-// TODO: Export the factorial function
+module.exports = { factorial };
